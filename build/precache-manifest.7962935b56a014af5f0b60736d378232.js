e04d8281cf6bdecb972b200f864cea1ec43afd41
Ramish <786alam.raamish@gmail.com>
2020-02-24 13:40:23 +0500

    adding files for github pages

---
+++ build/precache-manifest.7962935b56a014af5f0b60736d378232.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9d5e21ad2487a4126e36f75a98905e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "5429143b2fa957d72a5e",
+    "url": "/monster-rolodex/static/css/main.5cdcb924.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5429143b2fa957d72a5e",
+    "url": "/monster-rolodex/static/js/main.233d0ff5.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
