2c1af7e6777dab7843dc49a5d8839c3c4703ef08
Ramish <786alam.raamish@gmail.com>
2020-02-24 14:22:25 +0500

    Updates

---
+++ precache-manifest.9e0be38b57c4d2299b9b84f917973793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f99c91897825125d65cb75281ca3bac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a1f3cdde2259316838dc",
+    "url": "/monsters-rolodex/static/css/main.5cdcb924.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f3cdde2259316838dc",
+    "url": "/monsters-rolodex/static/js/main.a84c5b22.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);
